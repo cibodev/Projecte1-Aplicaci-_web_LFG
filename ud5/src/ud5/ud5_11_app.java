a3125fc1ca7468d86cda288d25bbfe8642a2f58c
Emma <emmapago97@gmail.com>
2021-02-05 20:39:00 +0100

    ud5_11_app ok

--- ud5/src/ud5/ud5_11_app.java
+++ ud5/src/ud5/ud5_11_app.java
@@ -3,30 +3,22 @@ import javax.swing.JOptionPane;
 public class ud5_11_app {
 	public static void main (String []args) {
 		
-		String dia="Lunes";
+		String dia=JOptionPane.showInputDialog("día de la semana");
 		
 		switch (dia) {
-		case "Lunes":
-			System.out.println("Hoy es día laboral"+dia);
-			
-		case "Martes":
-			System.out.println("Hoy es día laboral"+dia);
-			
+		case "Lunes":			
+		case "Martes":			
 		case "Miércoles":
-			System.out.println("Hoy es día laboral"+dia);
-			break;
 		case "Jueves":
-			System.out.println("Hoy es día laboral"+dia);
-			break;
-		case "Viernes":
-			System.out.println("Hoy es día laboral"+dia);
-			break;
+		case "Viernes":			
 		case "Sábado":
-			System.out.println("Hoy es día laboral"+dia);
+			System.out.println("Hoy es día laboral "+dia);
 			break;
 		case "Domingo":
-			System.out.println("Hoy es día festivo"+dia);
-			
+			System.out.println("Hoy es día festivo "+dia);
+			break;
+			default:
+			System.out.println("día de la semana");
 		}
 	}
 }
