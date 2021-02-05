23abeee5ad18a3b1eb97d5cd7b5dca85fd44ba64
Emma <emmapago97@gmail.com>
2021-02-05 20:41:14 +0100

    ud5_9_app_ok

--- ud5/src/ud5/ud5_9_app.java
+++ ud5/src/ud5/ud5_9_app.java
@@ -3,9 +3,12 @@ import javax.swing.JOptionPane;
 public class ud5_9_app {
 	public static void main (String []args) {
 		
-		for (int num=1;num<=100;num++) {
-			System.out.println("num+1");
-			
+		int num=1;
+		while (num<=100) {
+			System.out.println(num);
+			num++;
+		if ((num%2==0)&&(num%3==0))
+			System.out.println(num);
 		}
 	}
 }
