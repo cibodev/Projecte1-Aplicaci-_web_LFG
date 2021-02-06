5d904b97d692c8a5f26698cbbc496970980be936
Emma <emmapago97@gmail.com>
2021-02-06 20:30:14 +0100

    ud5_4_ok

--- ud5/src/ud5/ud5_4.java
+++ ud5/src/ud5/ud5_4.java
@@ -1,11 +1,11 @@
 package ud5;
 import javax.swing.JOptionPane;
-interface Emmapago{
 public class ud5_4 {
 	public static void main (String []args) {
 		
-		String texto=JOptionPane.showInputDialog("Número radio");
-		double numdouble=double.parsedouble(numString);
-		System.out.println("double: "+numdouble);
+		String texto=JOptionPane.showInputDialog("Área de un circulo");
+		double radio=Double.parseDouble(texto);
+		double area=Math.PI*Math.pow(radio,2);
+		System.out.println("El area del circulo es "+area+"");
 	}
-}
+	}
