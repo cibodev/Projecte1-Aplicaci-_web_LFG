b1e1fb86d56b10b64236d8d09a7e2da8ac737a3c
Emma <emmapago97@gmail.com>
2021-02-06 15:54:07 +0100

    ud5_5_ok

--- ud5/src/ud5/ud5_5.java
+++ ud5/src/ud5/ud5_5.java
@@ -3,7 +3,13 @@ import javax.swing.JOptionPane;
 public class ud5_5 {
 	public static void main (String []args) {
 		
-		String texto=JOptionPane.showInputDialog("Número");
-		int num=Integer.parseInt(texto);
-		System.out.println("")
+		String texto=JOptionPane.showInputDialog("numero");
+		int numero=Integer.parseInt(texto);
+
+		if(numero%2==0){
+			System.out.println("El número "+numero+"es divisible entre 2");
+		}else {
+			System.out.println("El número "+numero+" no es divisible entre 2");
+		}
+	}
 }
