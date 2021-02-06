5bb7571e6d3c39dd6fcc4b46cf0ac4ab580d7452
Emma <emmapago97@gmail.com>
2021-02-06 16:07:25 +0100

    ud5_12_app_ok

--- ud5/src/ud5/ud5_12_app.java
+++ ud5/src/ud5/ud5_12_app.java
@@ -3,7 +3,15 @@ import javax.swing.JOptionPane;
 public class ud5_12_app {
 	public static void main (String []args) {
 		
-		String contraseña="blablacar";
-		
-	}
+		String contraseña=JOptionPane.showInputDialog("Contraseña"); 
+		switch (contraseña) {
+		case "Blablacar":	
+			System.out.println("Enhorabuena");
+			break;
+				}
 }
+}
+
+
+		
+		
