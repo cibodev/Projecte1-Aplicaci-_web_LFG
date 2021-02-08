cc5baca200cb4bf37127d24c65348227c9e59252
Emma <emmapago97@gmail.com>
2021-02-08 20:51:57 +0100

    ud7

---
+++ ud7/src/ud7/ud7_1_app.java
@@ -0,0 +1,26 @@
+package ud7;
+
+import java.util.ArrayList;
+
+public class ud7_1_app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Alumnos pertenecientes al curso de programación
+		ArrayList<String>lista=new ArrayList<>();
+		lista.add("aaa1");
+		lista.add("aaa1");
+		lista.add("aaa1");
+		lista.add("aaa1");
+		lista.add("aaa1");	
+		
+		for(Object o:lista) {
+			System.out.println(0);
+		}
+		
+
+	}
+
+}
