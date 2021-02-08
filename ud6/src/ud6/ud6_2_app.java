dec665de9ec4cba8205b738f0be94b6988bbfe00
Emma <emmapago97@gmail.com>
2021-02-08 12:59:13 +0100

    ud6_2_app

--- ud6/src/ud6/ud6_2_app.java
+++ ud6/src/ud6/ud6_2_app.java
@@ -1,14 +1,15 @@
 package ud6;
 import javax.swing.JOptionPane;
+import java.util.Random;
 public class ud6_2_app {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String texto=JOptionPane.showInputDialog("Números enteros aleatorios");
+		String texto=JOptionPane.showInputDialog("Introduce un número");
 		int num1=Integer.parseInt(texto);
-		String texto=JOptionPane.showInputDialog("Números enterosss aleatorios");
-		int num2=Integer.parseInt(texto);
-		
+        
+		int aleatorio=(int)(Math.random()*10000);
+		System.out.println(aleatorio);
 	}
 }
