bcc0ba1f7e77ff564fcbd73a1702dada5ae34660
Emma <emmapago97@gmail.com>
2021-02-09 10:32:52 +0100

    ud5_ok

--- ud5/src/ud5/ud5_10_app.java
+++ ud5/src/ud5/ud5_10_app.java
@@ -3,7 +3,14 @@ import javax.swing.JOptionPane;
 public class ud5_10_app {
 	public static void main (String []args) {
 		
-		String texto=JOptionPane.showInputDialog("Número de ventas");
+		String texto=JOptionPane.showInputDialog("Número");
+		
+				switch (número de ventas) {
+				
+				int numero=Integer.parseInt(texto);
+				}
+				
+		
 		
 	}
 }
