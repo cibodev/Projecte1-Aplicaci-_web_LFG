4bb4fbfa9509efb633514c48a31dc476ab69aca2
Emma <emmapago97@gmail.com>
2021-02-16 16:28:51 +0100

    ud6_1_ok

--- ud6/src/ud6/ud6_1_app.java
+++ ud6/src/ud6/ud6_1_app.java
@@ -1,44 +1,47 @@
 package ud6;
-import javax.swing.JOptionPane;
-public class ud6_1_app {
-	public static void main (String []args) {
-		
-		String figura=JOptionPane.showInputDialog("Figura: circulo, triangulo o cuadrado");
-		double resultado=0;
-		
-		switch(figura) {
-			case"circulo":
-				String circulo=JOptionPane.showInputDialog("Valores área circulo");
-				int radio=Integer.parseInt(circulo);
-				int resultado1=areaCirculo(radio);
-				break;
-		
-			case"triangulo":
-				String triangulo=JOptionPane.showInputDialog("Valores área triangulo");
-				int base=Integer.parseInt(triangulo);
-				int altura=Integer.parseInt(triangulo);
-				int resultado2=areaTriangulo(base,altura);
-				break;
-				
-			case"cuadrado":
-				String cuadrado=JOptionPane.showInputDialog("Valores área cuadrado");
-				int lado=Integer.parseInt(cuadrado);
-				int resultado3=areaCuadrado(lado);
-				break;
-			}
-	}
-	
-			public static int areaCirculo (int radio){
-				int resultado1=radio;
-				return resultado1;
-		}
-				
-			public static int areaTriangulo (int base, int altura) {
-				int resultado2=base*altura/2;
-				return resultado2;}
 
-			public static int areaCuadrado (int lado){
-				int resultado3=lado;
-				return resultado3;
-			}
+import javax.swing.JOptionPane;
+
+public class ud6_1_app {
+	public static void main(String[] args) {
+
+		String figura = JOptionPane.showInputDialog("Figura: circulo, triangulo o cuadrado");
+		double resultado = 0;
+
+		switch (figura) {
+		case "circulo":
+			String circulo = JOptionPane.showInputDialog("Introduce el radio");
+			int radio = Integer.parseInt(circulo);
+			resultado = areaCirculo(radio);
+			break;
+
+		case "triangulo":
+			String triangulo = JOptionPane.showInputDialog("Introduce la base");
+			int base = Integer.parseInt(triangulo);
+			String triangulo2 = JOptionPane.showInputDialog("Introduce la altura");
+			int altura = Integer.parseInt(triangulo);
+			resultado = areaTriangulo(base, altura);
+			break;
+
+		case "cuadrado":
+			String cuadrado = JOptionPane.showInputDialog("Introduce el lado");
+			int lado = Integer.parseInt(cuadrado);
+			resultado = areaCuadrado(lado);
+			break;
+		}
+	}
+
+	public static double areaCirculo(int radio) {
+		return (Math.pow(radio, 2)*Math.PI);
+	}
+
+	public static double areaTriangulo(int base, int altura) {
+		return base*altura/2;
+	}
+
+	public static int areaCuadrado(int lado) {
+		return lado*lado;
+	}
+
+	System.out.println("El área de" +figura+ "es" +resultado+);
 }
