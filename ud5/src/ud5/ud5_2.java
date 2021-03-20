0b590d6786cf1f111d292d4662f970cd603f749b
emmapago <78428230+emmapago@users.noreply.github.com>
2021-03-20 16:20:17 +0100

    Delete ud5 directory

--- ud5/src/ud5/ud5_2.java
+++
@@ -1,7 +0,0 @@
-package ud5;
-public class ud5_2 {
-	public static void main (String[]args) {
-	String cadena1="Emma"; {
-		System.out.println("Bienvenido "+cadena1);}
-	}
-}
--- ud5/src/ud5/ud5_3.java
+++
@@ -1,8 +0,0 @@
-package ud5;
-import javax.swing.JOptionPane;
-public class ud5_3 {
-	public static void main (String []args) {
-		String cadena1=JOptionPane.showInputDialog("Introducir nombre");
-		System.out.println("Bienvenido "+cadena1);
-	}
-}
