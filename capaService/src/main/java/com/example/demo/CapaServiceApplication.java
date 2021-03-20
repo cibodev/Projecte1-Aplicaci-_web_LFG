5a05b6dd04558ee1163849d895153be103d152e6
Emma <emmapago97@gmail.com>
2021-03-20 16:05:54 +0100

    capaService

---
+++ capaService/src/main/java/com/example/demo/CapaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapaServiceApplication.class, args);
+	}
+
+}
